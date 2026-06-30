f0e45c972901b87c95600ec1759e66c97bbf6128
neret <Neretinvitalie@yandex.ru>
2026-07-01 00:36:39 +0400

    -

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -6,30 +6,23 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Кнопка Поиск -> переход на SearchActivity
         val searchButton = findViewById<Button>(R.id.btn_search)
         searchButton.setOnClickListener {
-            val intent = Intent(this, SearchActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, SearchActivity::class.java))
         }
 
-        // Кнопка Медиатека -> переход на MediaActivity
         val mediaButton = findViewById<Button>(R.id.btn_media)
         mediaButton.setOnClickListener {
-            val intent = Intent(this, MediaActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, MediaActivity::class.java))
         }
 
-        // Кнопка Настройки -> переход на SettingsActivity
         val settingsButton = findViewById<Button>(R.id.btn_settings)
         settingsButton.setOnClickListener {
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, SettingsActivity::class.java))
         }
     }
 }
